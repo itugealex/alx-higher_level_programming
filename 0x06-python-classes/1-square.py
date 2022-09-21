c3db933ab8c2c32dcc65a52e31d367fae31ecab7
itugealex <itugealex@gmail.com>
2022-09-21 01:12:21 +0000

    second task

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,7 +1,10 @@
-#!/user/bin/pyton3
-"""Define class square"""
+#!/usr/bin/python3
+"""define a class square"""
+
 
 class Square:
+    """Represents the square"""
     def __inint__(self, size):
-        """Initialize a new square"""
+        """Initialize a new Square
+        args: size (int):size of new square"""
         self.__size = size
